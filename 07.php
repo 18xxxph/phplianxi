a8ed7e970147537782d3011896696dba6f94d9b2
18xxxph <49505197+18xxxph@users.noreply.github.com>
2019-05-09 08:49:44 +0800

    Add files via upload

---
+++ 07.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8" >
+<title>菜鸟教程（runoob.com)</title>
+</head>
+<body>
+
+
+<form action="welcome.php" method="post">
+名字:<input type="text" name="hxp" >
+年龄:<input type="text" name="age" >
+<input type="sumbit" value="提交" >
+</form>
+
+
+</body>
+</html>
