a97f5b93be7425ff65f6b150c5300a41f69de6a8
18xxxph <49505197+18xxxph@users.noreply.github.com>
2019-05-09 09:33:40 +0800

    Add files via upload

---
+++ 08.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>"运算符"</title>
+</head>
+
+<?php
+$x=10; 
+$y=6;
+echo ($x + $y); // 输出16
+echo '<br>';  // 换行
+ 
+echo ($x - $y); // 输出4
+echo '<br>';  // 换行
+ 
+echo ($x * $y); // 输出60
+echo '<br>';  // 换行
+ 
+echo ($x / $y); // 输出1.6666666666667
+echo '<br>';  // 换行
+ 
+echo ($x % $y); // 输出4
+echo '<br>';  // 换行
+ 
+echo -$x;
+
+?>
+
+<body>
+</body>
+</html>
