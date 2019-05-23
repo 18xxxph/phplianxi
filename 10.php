5590e94576f36b8448ec639ac7e1e236e9ed0314
18xxxph <49505197+18xxxph@users.noreply.github.com>
2019-05-23 09:46:52 +0800

    Add files via upload

---
+++ 10.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Swith语句</title>
+</head>
+
+
+<?php
+$favcolor="red";
+switch ($favcolor)
+{
+case "red":
+    echo "你喜欢的颜色是红色!";
+    break;
+case "blue":
+    echo "你喜欢的颜色是蓝色!";
+    break;
+case "green":
+    echo "你喜欢的颜色是绿色!";
+    break;
+default:
+    echo "你喜欢的颜色不是 红, 蓝, 或绿色!";
+}
+?>
+
+
+
+<body>
+</body>
+</html>
